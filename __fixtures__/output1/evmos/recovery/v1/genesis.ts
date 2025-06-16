a14b8375493a861eab8a55daf2b02eb90871c636
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-06-16 20:52:27 +0300

    Update genesis.ts

--- __fixtures__/output1/evmos/recovery/v1/genesis.ts
+++ __fixtures__/output1/evmos/recovery/v1/genesis.ts
@@ -5,7 +5,7 @@ import { JsonSafe } from "../../../json-safe";
 export const protobufPackage = "evmos.recovery.v1";
 /** GenesisState defines the recovery module's genesis state. */
 export interface GenesisState {
-  /** params defines all the paramaters of the module. */
+  /** params defines all the parameters of the module. */
   params: Params;
 }
 /** GenesisState defines the recovery module's genesis state. */
